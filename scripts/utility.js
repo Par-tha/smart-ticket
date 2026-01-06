e984f9e4d0e6c39073ac12296a2aa1a5509ead4b
Par-tha <parthamondal16.pm@gmail.com>
2026-01-07 00:24:01 +0600

    add total cost with javascript

--- scripts/utility.js
+++ scripts/utility.js
@@ -10,63 +10,54 @@ function showElementById(elementId){
 
 const allSeats = document.getElementsByClassName('seat-btn');
 let count = 0;
+let price = 0;
 
 for(const seats of allSeats){
     seats.addEventListener('click', function(e){
 
-        // document.getElementById('seat-number').innerText = seats.innerText;
-        // document.getElementById('class-name').innerText = 'Economy';
-        // document.getElementById('seat-price').innerText = '1400'
-
-        // const seatChoosen = document.getElementById('seat-shoosen').childNodes[1].innerText;
-        
-        // console.log(seatChoosen);
-
-        
+       
         const element = document.getElementById('seat-number');
         element.classList.add('hidden');
 
         const seatNumber = seats.innerText;
         
-        const seatChoosen = document.getElementById('all-seat-choosen');
+        const allSeatChoosen = document.getElementById('all-seat-choosen');
+        
         const div = document.createElement('div');
         const p = document.createElement('p');
         p.innerText = seatNumber;
         div.appendChild(p);
-        // seatChoosen.appendChild(div);
+        
 
         const p2 = document.createElement('p');
         p2.innerText = 'Economy';
         div.appendChild(p2);
-        // seatChoosen.appendChild(div);
+        
 
         const p3 = document.createElement('p');
         p3.innerText = '1400';
         div.appendChild(p3);
 
-        seatChoosen.appendChild(div);
+        allSeatChoosen.appendChild(div);
         
-        
-        // const p2 = document.createElement('p');
-        // p2.innerText = 'Economy';
-        // seatChoosen.appendChild(p2);
-        
-        
-        // const p3 = document.createElement('p');
-        // p3.innerText = '1400';
-        // seatChoosen.appendChild(p3);
-        
-       
+    
         count = count + 1;
        setInnerText('seat-count', count);
+       price = price + 1400;
+       setInnerText('total-cost', price)
+
 
     })
 }
+
+
 
 function setInnerText (id, value){
     // console.log(id , value)
     document.getElementById(id).innerText = value;
 }
+
+
 
 
 // function setBackgroundColor(elementId){
