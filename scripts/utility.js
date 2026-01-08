7453718d8523d5f6f315d3521640d39da7ce8798
Par-tha <parthamondal16.pm@gmail.com>
2026-01-08 23:17:46 +0600

    selected seat

--- scripts/utility.js
+++ scripts/utility.js
@@ -7,7 +7,7 @@ function showElementById(elementId){
     element.classList.remove('hidden')
 }
 
-
+let seatLeft = parseInt(document.getElementById('seat-left').innerText);
 const allSeats = document.getElementsByClassName('seat-btn');
 let count = 0;
 let price = 0;
@@ -15,14 +15,19 @@ let price = 0;
 for(const seats of allSeats){
     seats.addEventListener('click', function(e){
 
-       
+        // no seat choosen hide
         const element = document.getElementById('seat-number');
         element.classList.add('hidden');
 
         const seatNumber = seats.innerText;
         
         const allSeatChoosen = document.getElementById('all-seat-choosen');
+
+        // seat selected color
+        e.target.style.backgroundColor = '#84dc7661';
+        e.target.setAttribute ='selected';
         
+        // created seat, class and seat price in div
         const div = document.createElement('div');
         const p = document.createElement('p');
         p.innerText = seatNumber;
@@ -40,32 +45,35 @@ for(const seats of allSeats){
 
         allSeatChoosen.appendChild(div);
         
-    
+        // seat count
         count = count + 1;
        setInnerText('seat-count', count);
+       
+        // price
        price = price + 1400;
-       setInnerText('total-cost', price)
+       setInnerText('total-cost', price);
 
+        // seat left
+        
+        seatLeft = seatLeft - 1 ;
+        console.log(seatLeft)
+        setInnerText('seat-left', seatLeft);
 
+        
+        const totalPrice = parseInt(document.getElementById('total-cost').innerText);
+       if(totalPrice > 5600){
+        alert('Max Select Four Seats');
+        return ;
+       }
+       
+        
+       
+       
     })
 }
 
 
 
 function setInnerText (id, value){
-    // console.log(id , value)
     document.getElementById(id).innerText = value;
 }
-
-
-
-
-// function setBackgroundColor(elementId){
-//     const element = document.getElementById(elementId);
-//     element.classList.add('bg-orange-400');
-// }
-
-// function removeBackgroundColor(elementId){
-//     const element = document.getElementById(elementId);
-//     element.classList.remove('bg-orange-400');
-// }
